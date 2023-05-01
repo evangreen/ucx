3fa111981845b118f327126f32dbf4c09c76a93d
Evan Green <evan@rivosinc.com>
2023-05-01 15:43:25 -0700

    UCM/BISTRO/RISCV: Fix address calculations
    Loading a 64-bit address in via immediates on RISC-V is not at all
    straightforward. Not only are you restricted to immediate loads of 20
    and 12, but all the add operations are sign extending. This means every
    time you combine two pieces of your address, if the top bit is 1, it
    turns into a subtraction.
    
    Rather than doing all these error-prone calculations, it's simpler to
    just plunk the address in the text stream, do a pc-relative load, and
    jump to it. In both cases it's an indirect branch via register, takes
    the same amount of space, and will be accelerated by the branch
    predictor.
    
    Signed-off-by: Evan Green <evan@rivosinc.com>

--- src/ucm/bistro/bistro_rv64.c
+++ src/ucm/bistro/bistro_rv64.c
@@ -28,58 +28,17 @@
 #include <ucs/arch/cpu.h>
 #include <ucs/debug/assert.h>
 
-/* Registers numbers to use with the move immediate to register.
-  * The destination register is X31 (highest temporary).
-  * Register X28-X30 are used for block shifting and masking.
-  * Register X0 is always zero
-  */
- #define X31 31
- #define X30 30
- #define X0  0
-
- /**
-  * @brief JALR - Add 12 bit immediate to source register, save to destination register, jump and link from destination register
-  *
-  * @param[in] _reg  register number (0-31), @param[out] _reg register number (0-31), @param[imm] 12 bit immmediate value
-  */
- #define JALR(_regs, _regd, _imm) (((_imm) << 20) | ((_regs) << 15) | (0b000 << 12) | ((_regd) << 7) | (0x67))
-
- /**
-  * @brief ADDI - Add 12 bit immediate to source register, save to destination register 
-  *
-  * @param[in] _reg  register number (0-31), @param[out] _reg register number (0-31), @param[imm] 12 bit immmediate value
-  */
- #define ADDI(_regs, _regd, _imm) (((_imm) << 20) | ((_regs) << 15) | (0b000 << 12) | ((_regd) << 7) | (0x13))
- #define ADD(_regs_a, _regs_b, _regd) ((_regs_b << 20) | (_regs_a << 15) | (0b000 << 12) | ((_regd) << 7) | (0x33))
-
- /**
-  * @brief LUI - load upper 20 bit immediate to destination register
-  *
-  * @param[in] _reg  register number (0-31), @param[out] _reg register number (0-31), @param[imm] 12 bit immmediate value
-  */
- #define LUI(_regd, _imm) (((_imm) << 12) | ((_regd) << 7) | (0x37))
-
- /**
-  * @brief SLLI - left-shift immediate number of bits in source register into destination register
-  *
-  * @param[in] _reg  register number (0-31), @param[out] _reg register number (0-31), @param[imm] 12 bit immmediate value
-  */
- #define SLLI(_regs, _regd, _imm) (((_imm) << 20) | ((_regs) << 15) | (0b001 << 12) | ((_regd) << 7) | (0x13))
-
 ucs_status_t ucm_bistro_patch(void *func_ptr, void *hook, const char *symbol,
                               void **orig_func_p,
                               ucm_bistro_restore_point_t **rp)
 {
     ucs_status_t status;
-    uintptr_t hookp = (uintptr_t)hook;
-
     ucm_bistro_patch_t patch = {
-        .rega = LUI  (X31, ((0xFFFFF << 12) & ( ((hookp) >> 32) + 1 ) ) >> 12),
-        .regb = ADDI (X31, X31, ((0xFFF)    & ( ((hookp) >> 32) + 1 ) )      ),
-        .regc = LUI  (X30, ((0xFFFFF << 12) & ( (((hookp)) + 1)     ) ) >> 12),
-        .regd = SLLI (X31, X31, 32),
-        .rege = ADD  (X30, X31, X31),
-        .regf = JALR (X31, X0, ((0xFFF)     & ( ((hookp)) + 1)))
+        .auipc = 0x00000f97, /* auipc x31, 0 */
+        .ld = 0x010f3f83, /* ld x31, 0x10(0x30) */
+        .jalr = 0x00000fe7, /* jalr x31, x0, 0 */
+        .spare = 0,
+        .address = (uintptr_t)hook
     };
 
     if (orig_func_p != NULL) {
